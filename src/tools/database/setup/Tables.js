665d82c7d2550986e3bea09bfaed704066b6bac3
mousezero <rm@mousezero.com>
2017-02-24 20:44:37 -0800

    Now code can build the "times" table"

--- src/tools/database/setup/Tables.js
+++ src/tools/database/setup/Tables.js
@@ -5,6 +5,26 @@ const pool = new pg.Pool(config);
 
 pool.connect( (err, client, done) => {
   if(err) return console.log('Error: ', err);
-  console.log('connected!!!!');
+
+  client.query(
+    `
+    CREATE TABLE public.times
+    (
+      id SERIAL,
+      note character(150),
+      project_id integer,
+      time_stamp timestamp without time zone DEFAULT now()
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    ALTER TABLE public.times
+    OWNER TO dev;
+    `,
+    [],
+    function (err){
+      return console.error(err);
+    }
+  )
   done();
 });
