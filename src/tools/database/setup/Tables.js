1c33ceb9f2a1d65f3ab60d09440b6be64609262b
mousezero <rm@mousezero.com>
2017-02-25 14:46:11 -0800

    table setup ends racefully and also exectues in the right order

--- src/tools/database/setup/Tables.js
+++ src/tools/database/setup/Tables.js
@@ -4,7 +4,7 @@ const config = require('../../../dpPoolConfig.js');
 const pool = new pg.Pool(config);
 
 pool.connect( (err, client, done) => {
-  if(err) return console.log('Error: ', err);
+  if(err) return console.log('Error: ', err)
 
   client.query(
     `
@@ -22,32 +22,31 @@ pool.connect( (err, client, done) => {
     OWNER TO dev;
     `,
     [],
-    function (err){
-      return console.error(err);
-    }
-  )
+    err => console.error(err))
+  .then(function(){
+    return client.query(
+      `
+      CREATE TABLE public.times
+      (
+        id SERIAL UNIQUE,
+        note character(150),
+        project_id integer,
+        time_stamp timestamp without time zone DEFAULT now(),
+        CONSTRAINT times_id_fkey FOREIGN KEY (id)
+            REFERENCES public.projects (id) MATCH SIMPLE
+            ON UPDATE NO ACTION ON DELETE NO ACTION
+      )
+      WITH (
+        OIDS=FALSE
+      );
+      ALTER TABLE public.times
+    OWNER TO dev;
+      `,
+      [],
+      err => console.error(err))
+  })
+  .then(function(){
+    process.exit()
+  })
 
-  client.query(
-    `
-    CREATE TABLE public.times
-    (
-      id SERIAL UNIQUE,
-      note character(150),
-      project_id integer,
-      time_stamp timestamp without time zone DEFAULT now(),
-      CONSTRAINT times_id_fkey FOREIGN KEY (id)
-          REFERENCES public.projects (id) MATCH SIMPLE
-          ON UPDATE NO ACTION ON DELETE NO ACTION
-    )
-    WITH (
-      OIDS=FALSE
-    );
-    ALTER TABLE public.times
-  OWNER TO dev;
-    `,
-    [],
-    function (err){
-      return console.error(err);
-    }
-  )
-});
+})
