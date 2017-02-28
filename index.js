e3971f374305d6ac5edd0d229454e731f8982bfe
mousezero <rm@mousezero.com>
2017-02-28 11:01:32 -0800

    App can create a jwt

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const apiRoutes = express.Router()
 const routes = require('./src/routes/api')
+const config = require('./config.json')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -15,8 +16,8 @@ app.get('/', function (req, res) {
   })
 })
 
-routes(apiRoutes)
-app.use('/api', apiRoutes)
+routes(apiRoutes, config.secret)
+app.use('/', apiRoutes)
 
 app.listen(PORT, function () {
   console.log(`Server is listening on port ${PORT}`)
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,8 +2,9 @@ const poolConfig = require('../dbPoolConfig')
 const Pool = require('pg').Pool
 const pool = new Pool(poolConfig)
 const times = require('../persistence/times')(pool)
+const jwt = require('jsonwebtoken')
 
-module.exports = function api (apiRoutes) {
+module.exports = function api (apiRoutes, secret) {
   apiRoutes.post('/punch', function (req, res) {
     times.getTimesList()
     .then(function (data) {
@@ -13,6 +14,17 @@ module.exports = function api (apiRoutes) {
         description: req.body.description,
         stuff: data
       })
+    })
+  })
+
+  apiRoutes.post('/authenticate', function (req, res) {
+    const token = jwt.sign({id: '.myId'}, secret, {
+      expiresIn: 86400
+    })
+    res.json({
+      success: true,
+      msg: 'test',
+      token: token
     })
   })
 }
